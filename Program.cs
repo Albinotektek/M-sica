2f08a3922fe98ce32ad1be20b4aa7f3f8b800f36
Albinotektek <162760294+Albinotektek@users.noreply.github.com>
2024-04-07 19:22:08 -0300

    musica

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.Gray;
+Console.WriteLine(@"Sweet Child O' Mine
+Guns N' Roses");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.WriteLine(@"She's got a smile that it seems to me
+Reminds me of childhood memories
+Where everything was as fresh
+As the bright blue sky
+
+Now and then when I see her face
+She takes me away to that special place
+And if I stare too long
+I'd probably break down and cry");
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(@"Oh, oh, oh, sweet child of mine
+Oh, oh, oh, oh, sweet love of mine");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.WriteLine(@"She's got eyes of the bluest skies
+As if they thought of rain
+I hate to look into those eyes
+And see an ounce of pain
+
+Her hair reminds me of a warm safe place
+Where, as a child, I'd hide
+And pray for the thunder and the rain
+To quietly pass me by");
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(@"Oh, oh, oh, sweet child of mine
+Oh, oh, oh, oh, sweet love of mine
+Oh, oh, oh, oh, sweet child of mine
+Ooh, yeah, yeah
+Ooh! Sweet love of mine");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.WriteLine(@"Where do we go?
+Where do we go now?
+Where do we go?
+
+Where do we go?
+Where do we go now?
+Where do we go now?
+
+Where do we go?
+Where do we go now?
+Where do we go?
+Where do we go?
+
+Where do we go now?
+Where do we go?
+Where do we go now?
+Where do we go? (2x)
+Now, now, now
+Now, now, now, now?");
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine(@"Sweet child
+Sweet child of mine");
+Console.ResetColor();
+
+Console.WriteLine();
